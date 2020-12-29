08d1394bdf45f5a274ca7ddcfa6dd09bf4187bfe
Tazkia Kamila <tami.tazkia@gmail.com>
2020-12-29 14:33:09 +0700

    added router to app.js customized city names in header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,14 @@ function Header() {
   return (
     <header>
       <div>
-        <h1> TODO </h1>
+        <h1> Tami's Weather App </h1>
       </div>
       <nav>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
+        <a href="/?city=Jakarta"> Jakarta </a>
+        <a href="/?city=Bandung"> Bandung </a>
+        <a href="/?city=Sydney"> Sydney </a>
+        <a href="/?city=Canada"> Canada </a>
+        <a href="/?city=France"> France </a>
       </nav>
     </header>
   );
