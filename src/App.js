09dfa230c0df280c322c4af8da2e268536aca6b0
Tazkia Kamila <tami.tazkia@gmail.com>
2020-12-31 15:53:57 +0700

    adding Details.js, changing App.js and Home.js, Home.js has the wrong code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 import Home from "./containers/Home";
+import Details from "./containers/Details";
 
 function App() {
   return (
     <Router>
       <Switch>
+        {/* Details Route */}
+        <Route path="/city">
+          <Details />
+        </Route>
+
         <Route path="/">
           <Home />
         </Route>
